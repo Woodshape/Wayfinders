427342f972a3d3c9a4ef28994aa3903d6ee11f70
Woodshape <rkarschny@gmail.com>
2019-11-03 12:57:20 +0100

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform gunHand;
+    public float moveSpeed;
+
+    private Rigidbody2D _myRigidbody;
+
+    private Vector2 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        _myRigidbody.velocity = moveInput * moveSpeed;
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerPos = Camera.main.WorldToScreenPoint(transform.localPosition);
+
+        //  Flip the player to look the way the mouse if facing.
+        if (mousePos.x < playerPos.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunHand.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunHand.localScale = Vector3.one;
+        }
+
+        // Rotate the gun hand.
+        Vector2 offset = new Vector2(mousePos.x - playerPos.x, mousePos.y - playerPos.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+        gunHand.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
